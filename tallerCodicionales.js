1aa0e1ba52fe4fffa47581bff51f17426af73884
sofisebs <sofilopeznazareth@gmail.com>
2022-04-27 08:12:52 -0500

    Tallercd

---
+++ tallerCodicionales.js
@@ -0,0 +1,92 @@
+//punto 1
+
+var saludar = "si";
+ 
+if(saludar=="si"){
+    console.log("Hola, soy un condicional simple");
+}
+
+//punto 2
+
+var x=20;
+ 
+if(x<=20){
+    console.log("La condición es verdadera");
+}
+
+//punto 3
+
+var edad =18; 
+ 
+if(edad>=18){	
+    console.log("Bienvenido, puede ingresar");
+}else{ 
+    (edad<=18)
+    console.log("Lo siento, no tiene edad suficiente para entrar aquí");
+}
+
+//Punto 4 
+
+var numero1 = 5;
+var numero2 = 8;
+ 
+if(numero1<numero2) {
+  console.log("numero1 no es mayor que numero2");
+}
+if(numero2>0) {
+  console.log("numero2 es positivo");
+}
+if(numero1 !== 0 ) {
+  console.log("numero1 es negativo o distinto de cero");
+}
+if(numero1+1<numero2) {
+  console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
+ 
+//punto 5
+
+var numerito = 100;
+ 
+if(numerito<50) {
+  console.log("numerito sí es estrictamente menor que 50");
+}else if(numerito>50 && numerito>80) {
+  console.log("numerito sí está entre 50 y 80");
+}else if(numerito>80 && numerito<=100 ) {
+  console.log("numerito es mayor que 80 pero no mayor ni igual a 100");
+}else if(numerito) {
+  console.log("numerito es igual a 100");
+}else if (numerito>100){
+    console.log("numerito es estrictamente mayor a 100");
+  }
+
+//punto 6 
+
+if(numerito==83) {
+  console.log("numerito es mayor que 80 pero no mayor ni igual a 100")
+}
+
+
+
+
+
+
+//punto 9
+
+var diaActual ="martes";
+
+
+
+
+//punto 10
+
+var valor1 =3;
+var valor2 =8;
+
+if(valor2 == 0){
+  console.log("no se puede dividir")
+}else{
+  console.log(valor1/valor2)
+}
+
+
+
